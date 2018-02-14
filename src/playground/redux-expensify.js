bac5ba41e58ef7cd7909d38449c587260e98f68c
bonecrushereb <bonecrushereb@gmail.com>
2018-02-14 09:09:14 -0800

    working on combine reducers for expensify app

---
+++ src/playground/redux-expensify.js
@@ -0,0 +1,54 @@
+import { createStore, combineReducers } from 'redux';
+
+//Expenses Reducer
+
+const expenseReducerDefaultState = [];
+
+const expensesReducer = (state = expenseReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+//Filters Reducer
+const filtersReducerDefaultState = {
+  text: '',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate: undefined
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+//Store Creation
+
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+  })
+);
+
+console.log(store.getState());
+
+const demoState = {
+  expenses: [{
+    id: 'adfadfdf',
+    description: 'January Rent',
+    note: 'This was the final payment for that address',
+    amount: 54500,
+    createdAt: 0
+  }],
+  filters: {
+    text: 'rent',
+    sortBy: 'amount',
+    startDate: undefined,
+    endDate: undefined
+  }
+};
