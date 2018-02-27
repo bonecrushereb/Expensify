7e8e0f8ffbeb991474977b05f7c45249c77e6bdd
bonecrushereb <bonecrushereb@gmail.com>
2018-02-27 13:12:08 -0800

    add firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const config = {
+   apiKey: "AIzaSyAeoKmrfHJvQp1j2D-LUtrhsnJ1xurLA9I",
+   authDomain: "expensify-539e1.firebaseapp.com",
+   databaseURL: "https://expensify-539e1.firebaseio.com",
+   projectId: "expensify-539e1",
+   storageBucket: "expensify-539e1.appspot.com",
+   messagingSenderId: "1026242339669"
+ };
+
+ firebase.initializeApp(config);
