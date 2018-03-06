8513ce6e253d0b68b763157896af97ab627ae7cc
bonecrushereb <bonecrushereb@gmail.com>
2018-03-06 11:53:04 -0800

    seperated test and development databases

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-   apiKey: "AIzaSyAeoKmrfHJvQp1j2D-LUtrhsnJ1xurLA9I",
-   authDomain: "expensify-539e1.firebaseapp.com",
-   databaseURL: "https://expensify-539e1.firebaseio.com",
-   projectId: "expensify-539e1",
-   storageBucket: "expensify-539e1.appspot.com",
-   messagingSenderId: "1026242339669"
+   apiKey: process.env.FIREBASE_API_KEY,
+   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+   databaseURL: process.env.FIREBASE_DATABASE_URL,
+   projectId: process.env.FIREBASE_PROJECT_ID,
+   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
  };
 
  firebase.initializeApp(config);
